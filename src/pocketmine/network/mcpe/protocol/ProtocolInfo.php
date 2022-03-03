9a9aa6c32dd5d5e7d13f9ad0dd26e197103e436b
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2022-03-03 15:09:13 +0100

    MultiProtocol 475, 486

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -37,11 +37,15 @@ interface ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 486;
+	public const CURRENT_PROTOCOL = self::PROTOCOL_1_18_10;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
-	public const MINECRAFT_VERSION = 'v1.18.10';
+	public const MINECRAFT_VERSION = 'v1.18.X';
 	/** Version number sent to clients in ping responses. */
 	public const MINECRAFT_VERSION_NETWORK = '1.18.10';
+
+	public const PROTOCOL_1_18_0 = 475;
+	public const PROTOCOL_1_18_10 = 486;
+	public const ACCEPTED_PROTOCOL = [self::PROTOCOL_1_18_0, self::PROTOCOL_1_18_10];
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
